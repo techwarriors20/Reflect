8c831bab48c99362622384a720de387aba515df3
techwarriors <techwarriors20@gmail.com>
2020-05-27 23:57:51 +0530

    Domain and Infra class libarary projects added

---
+++ Reflect.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Reflect.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Reflect.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Reflect.Infra
+{
+    public class Class1
+    {
+    }
+}
