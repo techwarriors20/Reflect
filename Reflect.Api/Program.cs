14022777a4bed5dd2fd6dd560f9aafebb1f864ad
techwarriors20 <techwarriors20@gmail.com>
2020-07-22 22:58:53 +0530

    External configuration - comments added.

--- Reflect.Api/Program.cs
+++ Reflect.Api/Program.cs
@@ -25,11 +25,14 @@ namespace Reflect.Api
                 var settings = config.Build();
                 config.AddAzureAppConfiguration(options =>
                 {
+                    // fetch connection string from local config. Could use KeyVault, or Secrets as well.
                     options.Connect(settings["ConnectionStrings:AppConfig"])
                            .ConfigureRefresh(refresh =>
                            {
+                               // When this value changes, on the next refresh operation, the config will update all modified configs since it was last refreshed.
                                refresh.Register("Reflect.Api:Settings:Sentinel", refreshAll: true)
                                       .SetCacheExpiration(new TimeSpan(0, 5, 0));
+                               // Set a timeout for the cache so that it will poll the azure config every X timespan.
                            });
                 });
             })
